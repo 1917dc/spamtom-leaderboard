8d29445dc99530546c1547e22e17d3ce1f11bfde
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2025-03-29 17:39:57 -0300

    creating routes "home" and "convidados"

---
+++ src/routes/convidados/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    /** @type {{ data: import('./$types').PageData }} */
+    let { data } = $props();
+</script>
+
+<svelte:head>
+    <title>Convidados</title>
+</svelte:head>
+<main>
+
+</main>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    /** @type {{ data: import('./$types').PageData }} */
+    let { data } = $props();
+</script>
+
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
+<main>
+
+</main>
